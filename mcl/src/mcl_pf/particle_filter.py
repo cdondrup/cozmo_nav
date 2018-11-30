2ebbdd79d79ab97af60ee19e9c1608bf87cb7a39
Christian Dondrup <c.dondrup@hw.ac.uk>
2018-11-30 11:07:13 +0000

    Cleaning code and stopping subscribers and thread before initialising

--- mcl/src/mcl_pf/particle_filter.py
+++ mcl/src/mcl_pf/particle_filter.py
@@ -16,6 +16,13 @@ class ParticleFilter(object):
         self.weights = None
 
     def initialise(self, pose, pose_sigma=.1, rot_sigma=.2):
+        """Intialise the particle filter using a normal distribution.
+
+        :param pose: The geometry_msgs/PoseStamped that forms the centre of the normal distribution
+        :param pose_sigma: The std dev of the normal distribution for the x, y coordinates (default=0.1)
+        :param pose_sigma: The std dev of the normal distribution for the rotation in radians (default=0.2)
+        :returns: A triple of lists that contain the generate x, y coordinates and the angles theta
+        """
         rospy.loginfo("Initialising particle filter at ({}, {}, {}) - ({}, {}, {}, {})".format(
             pose.pose.position.x,
             pose.pose.position.y,
@@ -29,18 +36,43 @@ class ParticleFilter(object):
         return self.particles_x, self.particles_y, self.particles_t
 
     def random_sample_pose(self, pose, n, pose_sigma=.1, rot_sigma=.2):
+        """Generates random samples from a given pose using a normal distribution.
+
+        :param pose: The geometry_msgs/PoseStamped that forms the centre of the normal distribution
+        :param n: The number of particles to generate
+        :param pose_sigma: The std dev of the normal distribution for the x, y coordinates (default=0.1)
+        :param pose_sigma: The std dev of the normal distribution for the rotation in radians (default=0.2)
+        :returns: A triple of lists that contain the generate x, y coordinates and the angles theta
+        """
         x = pose.pose.position.x
         y = pose.pose.position.y
         _, _, yaw = euler_from_quaternion((pose.pose.orientation.x, pose.pose.orientation.y, pose.pose.orientation.z, pose.pose.orientation.w))
         return self.random_sample(x, y, yaw, n, pose_sigma, rot_sigma)
 
     def random_sample(self, x, y, a, n, pose_sigma=.1, rot_sigma=.2):
+        """Generates random samples from a given 2D position and angle using a normal distribution.
+
+        :param x: The x-coordinate of the centre of the normal destribution
+        :param y: The y-coordinate of the centre of the normal destribution
+        :param a: The rotation
+        :param n: The number of particles to generate
+        :param pose_sigma: The std dev of the normal distribution for the x, y coordinates (default=0.1)
+        :param pose_sigma: The std dev of the normal distribution for the rotation in radians (default=0.2)
+        :returns: A triple of lists that contain the generate x, y coordinates and the angles theta
+        """
         x = np.random.normal(x, pose_sigma, n)
         y = np.random.normal(y, pose_sigma, n)
         t = np.random.normal(a, rot_sigma, n)
         return x, y, t
 
     def update(self, dx, dy, da, dt):
+        """Update to constant motion model.
+
+        :param dx: The change in x direction since the last time step
+        :param dy: The change in y direction since the last time step
+        :param da: The change in rotation since the last time step
+        :param dt: The length of the interval over which this change in position happened
+        """
         factor = 1./dt
         self.dx = dx * factor
         self.dy = dy * factor
@@ -48,12 +80,26 @@ class ParticleFilter(object):
         rospy.logdebug("Update: {}, {}, {}".format(self.dx, self.dy, self.da))
 
     def predict(self, dt):
+        """Predicting the new position at the given time based on the constant velocity model.
+
+        :param dt: The time since the last update
+        :returns: A triple of lists that contain the generate x, y coordinates and the angles theta
+        """
         self.particles_x += (self.dx * dt)
         self.particles_y += (self.dy * dt)
         self.particles_t += (self.da * dt)
         return self.particles_x, self.particles_y, self.particles_t
 
     def observe(self, x, y, t, pose_sigma=.1, rot_sigma=.2):
+        """Making a new observation and changing the particle weights based on distance using a normal distribution.
+
+        :param x: The x-coordinate of the new observation.
+        :param y: The y-coordinate of the new observation.
+        :param t: The rotation of the new observation.
+        :param pose_sigma: The std dev of the normal distribution for the x, y coordinates (default=0.1)
+        :param pose_sigma: The std dev of the normal distribution for the rotation in radians (default=0.2)
+        :returns: The updated list of weights.
+        """
         xw = norm.pdf(self.particles_x, x, pose_sigma)
         yw = norm.pdf(self.particles_y, y, pose_sigma)
         tw = norm.pdf(self.particles_t, t, rot_sigma)
@@ -63,7 +109,10 @@ class ParticleFilter(object):
 
         return self.weights
 
-    def resample(self, pose, pose_sigma=.1, rot_sigma=.2, starvation_factor=0.2):
+    def resample(self, pose_sigma=.1, rot_sigma=.2, starvation_factor=0.2):
+        """Resamples the current particles based on the weights calculated during the last observation.
+
+        """
         n = int(np.round(self.num_particles*(1.-starvation_factor)))
         new_particles = np.random.choice(range(self.num_particles), replace=True, size=n, p=self.weights)
         x, y, t = self.random_sample(np.mean(self.particles_x), np.mean(self.particles_y), np.mean(self.particles_t), self.num_particles-n, pose_sigma, rot_sigma)
